4baca9673731cf89cd29bf1f29264cbbc410b09a
David Cady <cadykd@gmail.com>
2022-05-26 15:29:58 -0700

    My ship flies now.

--- js/manual-animation.js
+++ js/manual-animation.js
@@ -1,36 +1,72 @@
+const buttons = document.querySelectorAll("button");
+let rotateVal = 0;
+
+for(let value of buttons) {
+	let currentButton = value;
+	currentButton.addEventListener('mouseenter', () => changeColor(event.target));
+	currentButton.addEventListener('mouseleave', () => backToNormal(event.target));
+	currentButton.addEventListener('click', ()=> moveImage(event.target.id))
+}
+
+function changeColor(eventTarget) {
+	eventTarget.style.backgroundColor = "red";
+}
+
+function changeToWhite() {
+	eventTarget.style.backgroundColor = "white";
+}
+
+function backToNormal(eventTarget) {
+	eventTarget.style.backgroundColor='';
+}
 
 function moveImage(direction) {
-    let imgObjStyle = imgObj.style;
-    let topVal = parseInt(imgObjStyle.top, 10);
-    let leftVal = parseInt(imgObjStyle.left, 10);
+	let imgObjStyle = imgObj.style;
+	let topVal = parseInt(imgObjStyle.top, 10);
+	let leftVal = parseInt(imgObjStyle.left, 10);
 
-    if(direction === "left") {
-        imgObjStyle.left = (leftVal - 30) + "px";
-        console.log("moving left")
-    } else if(direction === "up") {
-        console.log("moving right")
-        imgObjStyle.top = (topVal - 30) + "px";
-    } else if(direction === "right") {
-        console.log("moving right")
-        imgObjStyle.left = (leftVal + 30) + "px";
-    } else if(direction === "down") {
-        console.log("moving down")
-        imgObjStyle.top = (topVal + 30) + "px";
+	if(direction === 'KeyA') {
+		imgObjStyle.transform = "rotate(270deg)";
+		imgObjStyle.left = (leftVal - 30) + "px";
+	}
+
+	if(direction === 'KeyW') {
+		imgObjStyle.transform = "rotate(0deg)";
+		imgObjStyle.top = (topVal - 30) + "px";
+	}
+
+	if(direction === 'KeyD') {
+		imgObjStyle.transform = "rotate(90deg)";
+		imgObjStyle.left = (leftVal + 30) + "px";
+	}
+
+	if(direction === 'KeyS') {
+		imgObjStyle.transform = "rotate(180deg)";
+		imgObjStyle.top = (topVal + 30) + "px";
+	}
+
+    if(direction === 'KeySpaceR') {
+    	rotateVal += 90;
+    	if (rotateVal === 360) {
+    		rotateVal = 0;
+    	}
+    	imgObjStyle.transform = "rotate(" + rotateVal + "deg)";
+    }
+
+    if(direction === 'KeySpaceL') {
+    	rotateVal -= 90;
+    	if (rotateVal === 360) {
+    		rotateVal = 0;
+    	}
+    	imgObjStyle.transform = "rotate(" + rotateVal + "deg)";
     }
 }
 
 function init() {
-    // select the image from the DOM
-    imgObj = document.getElementById('myImage');
-    // alter CSS style properties
-    imgObj.style.position = 'relative';
-    imgObj.style.left = '0px';
-    imgObj.style.top = '0px';
-    imgObj.style.top = '0px';
-    // alter imgObj
-    moveImage('right');
-    moveImage("right")
+	imgObj = document.getElementById('myImage');
+	imgObj.style.position = 'relative';
+	imgObj.style.left = '0px';
+	imgObj.style.top = '0px';
 }
-
 
 window.onload = init;
